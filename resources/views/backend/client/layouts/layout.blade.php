8a00b2b20eb5c99c5d507299f49991ed58a4ba10
mmaheen <mushrifmaheen@gmail.com>
2025-05-18 17:10:02 +0600

    Layout Manipulation

--- resources/views/backend/client/layouts/layout.blade.php
+++ resources/views/backend/client/layouts/layout.blade.php
@@ -13,21 +13,20 @@
       <meta name="description" content="">
       <meta name="author" content="">
       <!-- site icon -->
-      <link rel="icon" href="{{ asset('assets/backend/client') }}/images/fevicon.png" type="image/png" />
+      <link rel="icon" href="{{ asset('assets/backend/client/images/fevicon.png') }}" type="image/png" />
       <!-- bootstrap css -->
-      <link rel="stylesheet" href="{{ asset('assets/backend/client') }}/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="{{ asset('assets/backend/client/css/bootstrap.min.css') }}" />
       <!-- site css -->
-      <link rel="stylesheet" href="{{ asset('assets/backend/client') }}/style.css" />
+      <link rel="stylesheet" href="{{ asset('assets/backend/client/style.css') }}" />
       <!-- responsive css -->
-      <link rel="stylesheet" href="{{ asset('assets/backend/client') }}/css/responsive.css" />
+      <link rel="stylesheet" href="{{ asset('assets/backend/client/css/responsive.css') }}" />
       <!-- color css -->
-      <link rel="stylesheet" href="{{ asset('assets/backend/client') }}/css/colors.css" />
       <!-- select bootstrap -->
-      <link rel="stylesheet" href="{{ asset('assets/backend/client') }}/css/bootstrap-select.css" />
+      <link rel="stylesheet" href="{{ asset('assets/backend/client/css/bootstrap-select.css') }}" />
       <!-- scrollbar css -->
-      <link rel="stylesheet" href="{{ asset('assets/backend/client') }}/css/perfect-scrollbar.css" />
+      <link rel="stylesheet" href="{{ asset('assets/backend/client/css/perfect-scrollbar.css') }}" />
       <!-- custom css -->
-      <link rel="stylesheet" href="{{ asset('assets/backend/client') }}/css/custom.css" />
+      <link rel="stylesheet" href="{{ asset('assets/backend/client/css/custom.css') }}" />
       @yield('style')
       <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -62,22 +61,22 @@
          </div>
       </div>
       <!-- jQuery -->
-      <script src="{{ asset('assets/backend/client') }}/js/jquery.min.js"></script>
-      <script src="{{ asset('assets/backend/client') }}/js/popper.min.js"></script>
-      <script src="{{ asset('assets/backend/client') }}/js/bootstrap.min.js"></script>
+      <script src="{{ asset('assets/backend/client/js/jquery.min.js') }}"></script>
+      <script src="{{ asset('assets/backend/client/js/popper.min.js') }}"></script>
+      <script src="{{ asset('assets/backend/client/js/bootstrap.min.js') }}"></script>
       <!-- wow animation -->
-      <script src="{{ asset('assets/backend/client') }}/js/animate.js"></script>
+      <script src="{{ asset('assets/backend/client/js/animate.js') }}"></script>
       <!-- owl carousel -->
-      <script src="{{ asset('assets/backend/client') }}/js/owl.carousel.js"></script> 
+      <script src="{{ asset('assets/backend/client/js/owl.carousel.js') }}"></script> 
 
       <!-- nice scrollbar -->
-      <script src="{{ asset('assets/backend/client') }}/js/perfect-scrollbar.min.js"></script>
+      <script src="{{ asset('assets/backend/client/js/perfect-scrollbar.min.js') }}"></script>
       <script>
          var ps = new PerfectScrollbar('#sidebar');
       </script>
       <!-- custom js -->
-      <script src="{{ asset('assets/backend/client') }}/js/custom.js"></script>
-      <script src="{{ asset('assets/backend/client') }}/js/chart_custom_style1.js"></script>
+      <script src="{{ asset('assets/backend/client/js/custom.js') }}"></script>
+      <script src="{{ asset('assets/backend/client/js/chart_custom_style1.js') }}"></script>
       @yield('scripts')
    </body>
 </html>
